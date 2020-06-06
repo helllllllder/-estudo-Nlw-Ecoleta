d406e44b9f93ccb01cb54ec62f691055483cc147
helllllllder <42891390+helllllllder@users.noreply.github.com>
2020-06-05 21:56:44 -0300

    #02 - Fix images not showing in mobile app

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -22,7 +22,7 @@ class PointsController {
         const serializedPoints = points.map(point => {
             return {
                 ...point,
-                image_url: `http://192.168.0.10/uploads/${point.image}`,
+                image_url: `http://192.168.0.10:3333/uploads/${point.image}`,
             };
         });
 
